211a6387f6ea2f7b9621547069cacb2906dc670d
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-20 02:37:42 +0300

    Update coinflip.js

--- Oyunlar/coinflip.js
+++ Oyunlar/coinflip.js
@@ -36,8 +36,8 @@ if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
                                         .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
                                         .setDescription(`In your wallet to place a bet ${para ? "just " + para + ' 💸 there is!' : 'you have no money!'}`))
         const result = [
+            "WINWIN"
             "LOOSELOOSE",
-          "WINWIN"
           "WINWIN"
          ] 
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
