7d3b5a70e139884089f3db842870770f3168ce07
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-19 08:30:14 +0300

    Update beercode.js

--- beercode.js
+++ beercode.js
@@ -29,7 +29,7 @@ require("./util/eventLoader.js")(client);
 
 //===============================================================================\\
 client.on("ready", async () => {
-  client.user.setActivity(`Discord Bot Games`, {
+  client.user.setActivity(`Zhelp | Zinvite`, {
     type: "PLAYING"
   });
 });
