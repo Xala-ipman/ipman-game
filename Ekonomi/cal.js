1ba6b2a9a45cf036f341a781d58e608d4acda48c
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-19 14:40:32 +0300

    Update cal.js

--- Ekonomi/cal.js
+++ Ekonomi/cal.js
@@ -79,7 +79,7 @@ await db.set(`çalma_${message.author.id}`, Date.now());
 }
 exports.conf = {
   enabled: true,
-  aliases: [],
+  aliases: ["st"],
 };
 //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
 exports.help = {
