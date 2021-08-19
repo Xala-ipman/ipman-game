cb6d6d4c5d22062e24cdc71f0e72f1bb1d84fc2b
Xala-ipman <89108413+Xala-ipman@users.noreply.github.com>
2021-08-20 01:46:04 +0300

    Create zebo.js

---
+++ Oyunlar/zebo.js
@@ -0,0 +1,93 @@
+const Discord = require('discord.js');
+var ayarlar = require("../ayarlar.json")
+const db = require('quick.db')
+exports.run = function(client, message,  args) {
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  let miktar = args[0]
+  let para = db.fetch(`para_${message.author.id}`)
+  if(!miktar) return message.channel.send(new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+      .setColor("GREEN")
+      .setDescription(`**slots** You Can't More Than<:emoji_24:870144185917902889> 50,000`))
+  //== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+if(miktar > 50000) return message.channel.send(new Discord.MessageEmbed()
+      .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+      .setColor("GREEN")
+      .setDescription(`**slots**You Can't More Than<:emoji_24:870144185917902889> 50,000`))
+  /////////////
+if(miktar > para) return message.channel.send(new Discord.MessageEmbed()
+  .setColor("#ffff00")
+  .setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+  .setDescription(`In your wallet to place a bet ${para ? "just " + para + ' 💸 there is!' : 'you have no money!'}`))
+  /////////////////
+  if(isNaN(miktar) || miktar < 0) return message.channel.send(new Discord.MessageEmbed()
+.setColor("RED")
+.setAuthor(message.author.tag, message.author.avatarURL({dynamic: true}))
+.setDescription(`🤔 The amount you entered is not a valid number !?`))
+ let kazandin = miktar*6  
+  let slots = ["<a:emoji_52:878045644252663880>","<a:emoji_51:878045629018955776>","","<a:emoji_52:878045644252663880>","<a:emoji_48:878045577898766356>","<a:emoji_50:878045611172188170>","<a:emoji_51:878045629018955776>","<a:emoji_50:878045611172188170>","<a:emoji_50:878045611172188170>","<a:emoji_47:878045564992901170>"];
+  //
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  let result1 = Math.floor((Math.random() * slots.length));
+  let result2 = Math.floor((Math.random() * slots.length));
+  let result3 = Math.floor((Math.random() * slots.length));
+
+  //
+  let ust1 = Math.floor((Math.random() * slots.length));
+  let ust2 = Math.floor((Math.random() * slots.length));
+  let ust3 = Math.floor((Math.random() * slots.length));
+
+  //
+  let alt1 = Math.floor((Math.random() * slots.length));
+  let alt2 = Math.floor((Math.random() * slots.length));
+  let alt3 = Math.floor((Math.random() * slots.length));
+  
+  
+//===BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\ 
+  if (slots[result1] === slots[result2] && slots[result2] === slots[result3] ) {
+    var text2 = `**
+    ___ZEBO___  
+|<a:emoji_55:878045689114927144><a:emoji_54:878045674418110585><a:emoji_53:878045661751279618>|${message.author.username}
+|                  |
+|                  |**`;
+          message.channel.send(text2).then(msg => {
+            setTimeout(() => {
+              msg.edit(`**
+    ___ZEBO___  
+|${slots[result1]}${slots[result2]}${slots[result3]}${message.author.username} 💵 __ 50000+ __
+|                  |
+|                  |**`,true);
+            }, 4500);
+          });
+
+     db.add(`para_${message.author.id}`, kazandin)
+//== BEERCODE (https://discord.gg/ew3dpTu4Z5) BEERCODE ==\\
+  } else {
+    var text3 = `**
+    ___SLOTS___  
+|<a:emoji_55:878045689114927144><a:emoji_54:878045674418110585><a:emoji_53:878045661751279618>|${message.author.username}
+|                  |
+|                  |**`;
+          message.channel.send(text3).then(msg => {
+            setTimeout(() => {
+              msg.edit(`**
+    ___ZEBO___  
+|${slots[result1]}${slots[result2]}${slots[result3]}|${message.author.username} 💵 __ 50000- __
+|                  |
+|                  |**`, true);
+            }, 4500);
+          });
+  db.add(`para_${message.author.id}`, -miktar)
+  }
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ["zebo","zo","Zebo"],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'Zebo',
+};
